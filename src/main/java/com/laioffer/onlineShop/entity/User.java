9ea286e2ec96a2df22979b50104c1180a84e6653
liuwuyayui <liuwuyayui@gmail.com>
2021-03-03 21:57:53 -0800

    Implemented Registration Service

--- src/main/java/com/laioffer/onlineShop/entity/User.java
+++ src/main/java/com/laioffer/onlineShop/entity/User.java
@@ -9,7 +9,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "users")
-public class User implements Serializable {
+public class  User implements Serializable {
   
   private static final long serialVersionUID = 2681531852204068105L;
   
